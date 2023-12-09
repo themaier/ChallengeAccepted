e8e636b5e47e95403aace9db99da738c9b25d623
Tobias <tobias.maier@st.oth-regensburg.de>
2023-12-09 10:46:06 +0100

    setup project

---
+++ backend/src/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+from src.db.session import init_db
+from fastapi.staticfiles import StaticFiles
+
+
+def app() -> FastAPI:
+    init_db()
+    app = FastAPI(
+        title="Escape-Room",
+        version="1.0.0",
+        swagger_ui_parameters={"tryItOutEnabled": True},
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    # app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    # app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
+
+    # app.mount("/resources", StaticFiles(directory="/backend/resources"), name="resources")
+    app.mount(
+        "/resources", StaticFiles(directory="../backend/resources"), name="resources"
+    )
+
+    @app.get("/", include_in_schema=False)
+    async def docs_redirect():
+        return RedirectResponse(url="/docs")
+
+    return app
+
+
+if __name__ == "__main__":
+    app()
