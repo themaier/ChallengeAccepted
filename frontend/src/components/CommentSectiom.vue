0d71f50c4e9e101a0eb1adaaf69f08c553c6e21f
Jeremias Rauwolf <82889934+jerreeey@users.noreply.github.com>
2024-01-13 19:45:22 +0100

    added images to comments

---
+++ frontend/src/components/CommentSectiom.vue
@@ -0,0 +1,49 @@
+<template>
+     <div v-for="challenge in challenges" :key="challenge.id" class="modal fade modal-lg" :id="'comment' + challenge.id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h2 class="modal-title h5" id="exampleModalLabel">Kommentare</h2>
+                    <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <ul style="max-height:500px; overflow: auto; " class="px-0 d-flex flex-column gap-3">
+                        <li v-for="comment in challenge.comments" :key="comment.id" class="d-flex">
+                            <span class="fw-bold  col-2">Tobi</span>
+                            <div class="col-10 d-flex flex-column gap-2 u.overflow-hidden">
+                                <img v-if="comment.prove_resource_path" class="rounded " :src="comment.prove_resource_path">
+                                <span v-if="comment.text">{{comment.text}}</span>
+                            </div>
+                        </li>
+                    </ul>
+                    <form>
+                        <div class="mb-3">
+                            <textarea class="form-control" id="exampleFormControlTextarea1" aria-label="Kommentieren" placeholder="Kommentieren..." rows="3"></textarea>
+                            <input class="form-control" id="formFile" type="file" name="image" accept="image/png, image/jpg, image/jpeg" capture="user">
+                        </div>
+                        <button class="btn btn-primary">Kommentieren</button>   	
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const { challenges } = defineProps(['challenges']);
+</script>
+
+<style lang="scss">
+img {
+    max-height:300px; 
+    width:auto; 
+    object-fit:contain; 
+    align-self:center;
+
+    @media screen and (max-width: 990px) {
+        max-width: 200px;
+    }
+}
+</style>
--- frontend/src/views/TrendingView.vue
+++ frontend/src/views/TrendingView.vue
@@ -26,36 +26,14 @@
             </div>
         </div>
     </div>
+<CommentSection :challenges="challenges"></CommentSection>
 
-    <div v-for="challenge in challenges" :key="challenge.id" class="modal fade" :id="'comment' + challenge.id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h2 class="modal-title h5" id="exampleModalLabel">Kommentare</h2>
-                    <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <ul style="max-height:500px; overflow: auto;" class="px-0">
-                        <li v-for="comment in challenge.comments" :key="comment.id" class="d-flex justify-content-between">
-                            <span class="fw-bold me-2">{{comment.username}}</span>
-                            <span>{{comment.text}}</span>
-                        </li>
-                    </ul>
-                    <form>
-                        <div class="mb-3">
-                            <textarea class="form-control" id="exampleFormControlTextarea1" aria-label="Kommentieren" placeholder="Kommentieren..." rows="3"></textarea>
-                        </div>
-                        <button class="btn btn-primary">Kommentieren</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
 import challengeService from '../services/challenge.service.js'
+import CommentSection from '../components/CommentSectiom.vue'
 import {useRoute} from 'vue-router'
 const route = useRoute()
 const like = ref(false)
